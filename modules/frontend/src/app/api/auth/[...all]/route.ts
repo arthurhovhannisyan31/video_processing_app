c005d5d50db140e75a9774235571b6ce915b3fbb
Arthur Hovhannisyan <arthurhovhannisyan31@gmail.com>
2026-07-26 18:26:44 +0400

    rename API_BASE_URL to NEXT_PUBLIC_BACKEND_BASE_URL
    - add error message reading with optional chaining to prevent runtime errors

--- modules/frontend/src/app/api/auth/[...all]/route.ts
+++ modules/frontend/src/app/api/auth/[...all]/route.ts
@@ -2,7 +2,7 @@ import type { AuthResponse } from "generated/client";
 
 import axios, { type AxiosResponse } from "axios";
 import { toNextJsHandler } from "better-auth/next-js";
-import { API_BASE_URL } from "configs/constants";
+import { NEXT_PUBLIC_BACKEND_BASE_URL } from "configs/constants";
 import { auth } from "lib/auth";
 import { type NextRequest, NextResponse } from "next/server";
 
@@ -22,7 +22,7 @@ export async function POST(request: NextRequest) {
       const { email, password } = await request.json();
 
       const response: AxiosAuthResponse = await axios.post<AuthResponse>(
-        `${API_BASE_URL}/auth/login`,
+        `${NEXT_PUBLIC_BACKEND_BASE_URL}/auth/login`,
         {
           email,
           password,
@@ -35,7 +35,7 @@ export async function POST(request: NextRequest) {
       const { email, password, name } = await request.json();
 
       const response: AxiosAuthResponse = await axios.post<AuthResponse>(
-        `${API_BASE_URL}/auth/register`,
+        `${NEXT_PUBLIC_BACKEND_BASE_URL}/auth/register`,
         {
           email,
           password,
--- modules/frontend/src/lib/auth-client.ts
+++ modules/frontend/src/lib/auth-client.ts
@@ -1,6 +1,3 @@
 import { createAuthClient } from "better-auth/react";
-import { API_BASE_URL } from "configs/constants";
 
-export const authClient = createAuthClient({
-  baseURL: API_BASE_URL,
-});
+export const authClient = createAuthClient();
