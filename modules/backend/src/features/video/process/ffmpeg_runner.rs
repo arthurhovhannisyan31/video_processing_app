c23e1b5d817268967acf532cea292b8d926f1b4b
Arthur Hovhannisyan <arthurhovhannisyan31@gmail.com>
2026-08-11 00:41:12 +0400

    add proper logs handling for ffmpeg output

--- modules/backend/src/features/video/process/ffmpeg_runner.rs
+++ modules/backend/src/features/video/process/ffmpeg_runner.rs
@@ -18,42 +18,43 @@ pub async fn ffmpeg_runner(
   let mut child_process = Command::new("ffmpeg")
     .kill_on_drop(true)
     .args(args)
-    .stdout(Stdio::piped())
+    .stdout(Stdio::null()) // FFmpeg output file mode doesn't use stdout
     .stderr(Stdio::piped())
     .spawn()
     .map_err(|err| ServerError::OtherError(anyhow!("Failed to spawn 'ffmpeg' process: {err:?}")))?;
 
-  let stdout = child_process
-    .stdout
-    .take()
-    .ok_or(ServerError::Processing("Missing ffmpeg stdout".to_string()))?;
   let stderr = child_process
     .stderr
     .take()
     .ok_or(ServerError::Processing("Missing ffmpeg stderr".to_string()))?;
 
-  let mut progress_lines = BufReader::new(stdout).lines();
   let mut error_lines = BufReader::new(stderr).lines();
 
-  let progress_task = tokio::spawn(async move {
-    while let Some(line) = progress_lines.next_line().await? {
-      // white to db
-      println!("Progress: {line:?}");
-    }
-    Ok(())
-  });
+  let log_task = tokio::spawn(async move {
+    let mut current_frame = "0".to_string();
+    let mut current_fps = "0".to_string();
 
-  let stderr_task = tokio::spawn(async move {
     while let Some(line) = error_lines.next_line().await? {
-      // write to db
-      println!("Progress: {line:?}");
+      let line = line.trim();
+
+      if let Some((key, value)) = line.split_once('=') {
+        match key {
+          "frame" => current_frame = value.to_string(),
+          "fps" => current_fps = value.to_string(),
+          "progress" => {
+            println!("DB update -> Frame: {current_frame}, FPS: {current_fps}, Status: {value}");
+          }
+          _ => {}
+        }
+      } else if !line.is_empty() {
+        println!("FFmpeg Log/Error: {}", line);
+      }
     }
     Ok(())
   });
 
   let status = child_process.wait().await?;
-  let _: Result<(), ServerError> = progress_task.await?;
-  let _: Result<(), ServerError> = stderr_task.await?;
+  let _: Result<(), ServerError> = log_task.await?;
 
   if !status.success() {
     return Err(ServerError::Processing(format!(
