df7f4b05dc097cf32cd3ccd1a627d2c0a16af624
Arthur Hovhannisyan <arthurhovhannisyan31@gmail.com>
2026-08-26 19:15:26 +0400

    add tokio timeout to ffmpeg binary calls to prevent binary failure issues

--- modules/backend/src/features/video/inspect/ffprobe_runner.rs
+++ modules/backend/src/features/video/inspect/ffprobe_runner.rs
@@ -1,10 +1,14 @@
+use std::process::Stdio;
+
 use serde_json::Value;
+use sqlx::__rt::timeout;
 use tokio::process::Command;
 
-use crate::core::error::ApplicationError;
+use crate::core::error::ServerError;
+use crate::features::video::constants::VIDEO_API_TIMEOUT;
 
-pub async fn ffprobe_runner(file_path: &str) -> Result<Value, ApplicationError> {
-  let output = Command::new("ffprobe")
+pub async fn ffprobe_runner(file_path: &str) -> Result<Value, ServerError> {
+  let ffprobe_process = Command::new("ffprobe")
     .kill_on_drop(true)
     .args([
       "-v",
@@ -15,19 +19,20 @@ pub async fn ffprobe_runner(file_path: &str) -> Result<Value, ApplicationError> 
       "json",
       file_path,
     ])
-    .output()
-    .await
-    .map_err(|err| {
-      ApplicationError::BadRequest(format!("Failed executing ffprobe binary: {err}",))
-    })?;
+    .stdout(Stdio::piped())
+    .spawn()
+    .map_err(|err| ServerError::Processing(format!("Failed to spawn 'ffprobe' process: {err}",)))?;
+
+  let output = match timeout(VIDEO_API_TIMEOUT, ffprobe_process.wait_with_output()).await {
+    Ok(output) => output?,
+    Err(_) => return Err(ServerError::Processing("ffmpeg timed out".to_string())),
+  };
 
   if !output.status.success() {
     let err_msg = String::from_utf8_lossy(&output.stderr).into_owned();
-    return Err(ApplicationError::BadRequest(format!(
-      "ffprobe error: {err_msg}"
-    )));
+    return Err(ServerError::Processing(format!("ffprobe error: {err_msg}")));
   }
 
   serde_json::from_slice(&output.stdout)
-    .map_err(|_| ApplicationError::Internal("Invalid JSON data from ffprobe".to_string()))
+    .map_err(|_| ServerError::Processing("Invalid JSON data from ffprobe".to_string()))
 }
--- modules/backend/src/features/video/process/ffmpeg_runner.rs
+++ modules/backend/src/features/video/process/ffmpeg_runner.rs
@@ -1,10 +1,11 @@
 use std::process::Stdio;
 
-use anyhow::anyhow;
 use tokio::io::{AsyncBufReadExt, BufReader};
 use tokio::process::Command;
+use tokio::time::timeout;
 
 use crate::core::error::ServerError;
+use crate::features::video::constants::VIDEO_API_TIMEOUT;
 
 pub async fn ffmpeg_runner(
   input: &str,
@@ -15,15 +16,15 @@ pub async fn ffmpeg_runner(
   args.extend(preset);
   args.extend([output]);
 
-  let mut child_process = Command::new("ffmpeg")
+  let mut ffmpeg_process = Command::new("ffmpeg")
     .kill_on_drop(true)
     .args(args)
     .stdout(Stdio::null()) // FFmpeg output file mode doesn't use stdout
     .stderr(Stdio::piped())
     .spawn()
-    .map_err(|err| ServerError::OtherError(anyhow!("Failed to spawn 'ffmpeg' process: {err:?}")))?;
+    .map_err(|err| ServerError::Processing(format!("Failed to spawn 'ffmpeg' process: {err:?}")))?;
 
-  let stderr = child_process
+  let stderr = ffmpeg_process
     .stderr
     .take()
     .ok_or(ServerError::Processing("Missing ffmpeg stderr".to_string()))?;
@@ -53,7 +54,10 @@ pub async fn ffmpeg_runner(
     Ok(())
   });
 
-  let status = child_process.wait().await?;
+  let status = match timeout(VIDEO_API_TIMEOUT, ffmpeg_process.wait()).await {
+    Ok(res) => res?,
+    Err(_) => return Err(ServerError::Processing("ffmpeg timed out".to_string())),
+  };
   let _: Result<(), ServerError> = log_task.await?;
 
   if !status.success() {
