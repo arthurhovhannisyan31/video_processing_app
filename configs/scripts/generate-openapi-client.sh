769608af881d397af6deda838e59917a4b32cab7
Arthur Hovhannisyan <arthurhovhannisyan31@gmail.com>
2026-08-28 20:03:42 +0400

    add correct dev server port for nginx dev config
    - add local openapi generation

--- configs/scripts/generate-openapi-client.sh
+++ configs/scripts/generate-openapi-client.sh
@@ -3,5 +3,9 @@
 REPO_ROOT="$(git rev-parse --show-toplevel)"
 
 yarn openapi-ts --client=@hey-api/client-axios \
-  -i "https://api.videoprocessing.app/api/openapi" \
+  -i "${REPO_ROOT}/modules/backend/openapi.json" \
   -o "${REPO_ROOT}/modules/frontend/src/generated/client"
+
+#yarn openapi-ts --client=@hey-api/client-axios \
+#  -i "https://api.videoprocessing.app/api/openapi" \
+#  -o "${REPO_ROOT}/modules/frontend/src/generated/client"
