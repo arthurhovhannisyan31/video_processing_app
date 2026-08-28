48450d4e913ed798fb97dd64e5d21ed322c45007
Arthur Hovhannisyan <arthurhovhannisyan31@gmail.com>
2026-08-29 00:00:00 +0400

    update deployment configs
    - rearrange memory resources in favor of backend
    - set nginx read timeout 300s to wait for BE
    - set nginx body size limit 10mb to not wait too long

--- modules/frontend/src/components/modules/video/constants.ts
+++ modules/frontend/src/components/modules/video/constants.ts
@@ -1,6 +1,6 @@
 import { formatBytes } from "lib/utils";
 
-export const DEFAULT_MAX_BODY_SIZE: number = 20 * 1024 * 1024;
+export const DEFAULT_MAX_BODY_SIZE: number = 10 * 1024 * 1024;
 export const supportedMimeTypes: string[] = ["video/mp4"];
 const supportedTypesLabel = supportedMimeTypes
   .map((el) => el.replace("image/", ""))
