1b8defed0e56c0d860cbaca057a20c6b5badb016
Arthur Hovhannisyan <arthurhovhannisyan31@gmail.com>
2026-09-05 04:20:20 +0400

    add x-user-id to backend server allowed headers

--- modules/backend/src/core/cors.rs
+++ modules/backend/src/core/cors.rs
@@ -1,10 +1,12 @@
 use std::sync::Arc;
 
 use axum::http::{HeaderValue, Method, header};
+use headers::HeaderName;
 use tower_http::cors::{AllowOrigin, CorsLayer};
 use tracing::log::warn;
 
 use crate::core::app_config::AppConfig;
+use crate::core::extractors::X_USER_ID_HEADER;
 
 pub fn build_cors_layer(app_config: Arc<AppConfig>) -> CorsLayer {
   let origin_values: Vec<HeaderValue> = app_config
@@ -17,6 +19,8 @@ pub fn build_cors_layer(app_config: Arc<AppConfig>) -> CorsLayer {
     })
     .collect();
 
+  let x_user_id_header = HeaderName::from_static(X_USER_ID_HEADER);
+
   CorsLayer::new()
     .allow_origin(AllowOrigin::list(origin_values))
     .allow_methods([
@@ -26,6 +30,11 @@ pub fn build_cors_layer(app_config: Arc<AppConfig>) -> CorsLayer {
       Method::PUT,
       Method::DELETE,
     ])
-    .allow_headers([header::AUTHORIZATION, header::CONTENT_TYPE, header::ACCEPT])
+    .allow_headers([
+      header::AUTHORIZATION,
+      header::CONTENT_TYPE,
+      header::ACCEPT,
+      x_user_id_header,
+    ])
     .allow_credentials(true)
 }
